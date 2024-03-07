bba18d91bef30f15dd6a385d8af9125857102c99
Egor <egor@kasadzhik.ru>
2024-03-07 15:23:58 +0300

    debug(container)

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-from flask import Flask,render_template
+def feb(n):
+    if n == 1 or n==2:
+        return 1    
+    else:
+        return feb(n-2) + feb(n-1)
 
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return '<a href="/zal">To the zals</a>'
-    return render_template("index.html")
-
-app.run(debug=True)
-@app.route('/zal')
-def zal():
-    return 'go to zal'
-
-@app.route('/licei')
-def licei():
-    return 'go to licei'
-
-if __name__ == '__main__':
-    app.run()
+print(feb(100))    
