79d032e4ef5103a196f7aa3b448d757292b5a050
Egor <egor@kasadzhik.ru>
2024-03-18 17:42:08 +0300

    xc

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-def feb(n):
-    if n == 1 or n==2:
-        return 1    
-    else:
-        return feb(n-2) + feb(n-1)
+from math import sqrt
+def f(x):
+    return -x**2 + 4
 
-print(feb(100))    
+def fp(a,b):
+    return abs((f(a) + f(b))/2 * (a-b))
+
+def L(a,b):
+    return sqrt((f(b)-f(a))**2 + (b-a)**2)
+
+a = -2
+b = 2
+n = 100
+h = (abs(a)+abs(b))/n
+s = 0
+l = 0
+for  i in range(0,n):
+    s += fp(a + i*h,a + (i+1)*h)
+    l += abs(L(a+i*h,a + (i+1)*h))
+
+print(s)
+print(l)      
